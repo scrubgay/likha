47666f3353947912caa0d14f2b1256135d47157d
Renz Torres <wr3ns@protonmail.com>
2022-08-11 06:58:31 -0400

    simplified js

--- likha.js
+++ likha.js
@@ -3,3 +3,23 @@ const { JSDOM } = jsdom;
 const fs = require('fs');
 const fm = require('front-matter');
 const {marked} = require('marked');
+
+const MDtoJSDOM = (filePath) => {
+    const file = fs.readFileSync(filePath, 'utf-8');
+    const parsedFile = fm(file); // splits out the front matter into body and attributes
+    parsedFile.body = marked.parse(parsedFile.body); // turns the markdown body into html plaintext
+    parsedFile.body = JSDOM.fragment(parsedFile.body); // turns the markdown text into a JSDOM fragment object
+
+    return parsedFile;
+}
+
+const constructPage = (content) => {
+    const template = fs.readFileSync('./template.html', 'utf-8');
+    const dom = new JSDOM(template);
+    const anchor = dom.window.document.querySelector("main");
+
+    anchor.appendChild(content);
+
+    return dom;
+}
+
