14076c5c8f2fed4237f7cb607c4bf5ca99621a21
Renz Torres <wr3ns@protonmail.com>
2022-08-24 09:36:07 -0400

    configurization of likha

--- likha.js
+++ likha.js
@@ -3,23 +3,48 @@ const { JSDOM } = jsdom;
 const fs = require('fs');
 const fm = require('front-matter');
 const {marked} = require('marked');
+const path = require('path');
 
-const MDtoJSDOM = (filePath) => {
+const configs = {
+    template: "/home/renz/Repositories/renzst.github.io/markdowns/template.html",
+    contentAnchor: "main",
+    sourceDir: "/home/renz/Repositories/renzst.github.io/markdowns/",
+    outputDir: "./dist/"
+}
+
+function MDtoJSDOM(filePath) {
     const file = fs.readFileSync(filePath, 'utf-8');
     const parsedFile = fm(file); // splits out the front matter into body and attributes
     parsedFile.body = marked.parse(parsedFile.body); // turns the markdown body into html plaintext
     parsedFile.body = JSDOM.fragment(parsedFile.body); // turns the markdown text into a JSDOM fragment object
 
     return parsedFile;
+};
+
+const Page = (content) => {
+    const template = fs.readFileSync(configs.template, 'utf-8');
+    const page = new JSDOM(template);
+
+    const main = page.window.document.querySelector(configs.contentAnchor);
+    
+    main.appendChild(content.body);
+
+    return page;
+};
+
+const main = () => {
+    const dir = fs.readdirSync(configs.sourceDir);
+    const files = dir.filter(x => x.includes(".md"));
+
+    for (let file of files) {
+        const name = file.slice(0, -3);
+        const content = MDtoJSDOM(path.join(configs.sourceDir, file));
+        const page = Page(content);
+
+        page.window.document.title = "renz torres | " + name;
+
+        fs.writeFileSync(path.join(configs.outputDir, name + ".html"), page.window.document.documentElement.outerHTML);
+    }
 }
 
-const constructPage = (content) => {
-    const template = fs.readFileSync('./template.html', 'utf-8');
-    const dom = new JSDOM(template);
-    const anchor = dom.window.document.querySelector("main");
-
-    anchor.appendChild(content);
-
-    return dom;
-}
-
+main();
